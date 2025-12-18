1ed1fd3dcc56602911c9be7a2843982a150e4779
kiirecodes <kirecodes@gmail.com>
2025-12-18 17:17:37 +0300

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,16 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
-pub fn handleComandError(command: []const u8) !void {
-    try stdout.print("{s}: command not found\n", .{command});
-}
+pub const Command = struct {
+    cmd: []const u8,
+    params: []const u8,
+
+    pub fn parseCmd(self: Command) !void {
+        if (std.mem.eql(u8, self.cmd, "echo")) {
+            try stdout.print("{s}\n", .{self.params});
+        } else try stdout.print("{s}: command not found\n", .{self.cmd});
+    }
+};
 
 pub fn main() !void {
     while (true) {
@@ -16,6 +23,26 @@ pub fn main() !void {
         try stdout.print("$ ", .{});
         const stream = try stdin.takeDelimiterExclusive('\n');
         if (std.mem.eql(u8, stream, "exit")) break;
-        try handleComandError(stream);
+
+        var _cmd_buffer: [50]u8 = undefined;
+        @memset(&_cmd_buffer, 0);
+        var i: usize = 0;
+        while (i < stream.len and stream[i] != ' ') : (i += 1) {
+            _cmd_buffer[i] = stream[i];
+        }
+        const _cmd = _cmd_buffer[0..i];
+
+        var params_buffer: [1024]u8 = undefined;
+        @memset(&params_buffer, 0);
+        i += 1;
+        var j: usize = 0;
+        while (i < stream.len) : (j += 1) {
+            params_buffer[j] = stream[i];
+            i += 1;
+        }
+        const params = params_buffer[0..j];
+
+        const cmd = Command{ .cmd = _cmd, .params = params };
+        try cmd.parseCmd();
     }
 }
