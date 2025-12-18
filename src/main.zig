4d8c4f4a36721794ed08ce97c80adc6a2d261efd
kiirecodes <kirecodes@gmail.com>
2025-12-18 15:24:24 +0300

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,18 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
-var buffer: [1024]u8 = undefined;
-var stdin_reader = std.fs.File.stdin().reader(&buffer);
-const stdin = &stdin_reader.interface;
-
 pub fn handleComandError(comand: []const u8) !void {
     try stdout.print("{s}: command not found\n", .{comand});
 }
 
 pub fn main() !void {
-    try stdout.print("$ ", .{});
-    const stream = try stdin.takeDelimiterExclusive('\n');
-    try handleComandError(stream);
+    while (true) {
+        var buffer: [1024]u8 = undefined;
+        var stdin_reader = std.fs.File.stdin().reader(&buffer);
+        const stdin = &stdin_reader.interface;
+
+        try stdout.print("$ ", .{});
+        const stream = try stdin.takeDelimiterExclusive('\n');
+        try handleComandError(stream);
+    }
 }
